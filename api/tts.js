8a19d9d8cf21ea92bceded4e9fac9c70fef331bd
rajandubey4u <rajandubey.4u@gmail.com>
2026-05-18 23:32:02 +0530

    Update tts.js

--- api/tts.js
+++ api/tts.js
@@ -16,7 +16,7 @@ export default async function handler(req, res) {
         },
         body: JSON.stringify({
             model: "tts-1",
-            voice: "nova",
+            voice: "shimmer",
             input: text
         })
     });
